38df28ceaa365eed7db9c4830ed2d8dbfb18025d
Osama Sakhi <msakhi21@gmail.com>
2019-12-27 11:40:02 -0500

    Publishing

---
+++ scripts/publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+printf "\n%s\n\n" "Deploying updates to GitHub..."
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Copy CNAME file in bc it keeps getting deleted.
+cp CNAME public/CNAME
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="Rebuilding site at: `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -am "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
